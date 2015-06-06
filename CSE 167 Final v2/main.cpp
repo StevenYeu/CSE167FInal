03205c4b0a7bbe76b4c0fc25f133d734e05cb234
stevenyeu <yeusteven@gmail.com>
2015-06-06 14:37:16 -0700

    Initial Commit

---
+++ CSE 167 Final v2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE 167 Final v2
+//
+//  Created by Steven Yeu on 6/6/15.
+//  Copyright (c) 2015 Steven Yeu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
