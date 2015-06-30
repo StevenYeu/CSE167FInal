4d60723d8ea8f604aca6a8db4a4e0df28886a898
stevenyeu <yeusteven@gmail.com>
2015-06-30 15:31:09 -0700

    CSE 167 Final

--- CSE 167 Final v2/main.cpp
+++ CSE 167 Final v2/main.cpp
@@ -1,15 +1,65 @@
-//
-//  main.cpp
-//  CSE 167 Final v2
-//
-//  Created by Steven Yeu on 6/6/15.
-//  Copyright (c) 2015 Steven Yeu. All rights reserved.
-//
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#ifdef _WIN32
+#include <GL/glut.h>
+#else
+#include <GLUT/glut.h>
+#endif
+
+#include "Window.h"
+#include "Cube.h"
+#include "Matrix4.h"
+#include "Globals.h"
+#include "MathTestBench.h"
+
+
+int main(int argc, char *argv[])
+{
+    //Math Test Bench
+    //MathTestBench::runTests();
+    
+    glutInit(&argc, argv);                                      //Initialize GLUT
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);   //Open an OpenGL context with double buffering, RGB colors, and depth buffering
+    glutInitWindowSize(Window::width, Window::height);          //Set initial window size
+    glutCreateWindow("UCSD CSE 167 - Final"); //Open window and set window title
+    glutFullScreen();           // making the window full screen
+
+    glEnable(GL_DEPTH_TEST);                                    //Enable depth buffering
+    glClear(GL_DEPTH_BUFFER_BIT);                               //Clear depth buffer
+    glClearColor(0.0, 0.0, 0.0, 0.0);                           //Set clear color to black
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);                  //Set polygon drawing mode to fill front and back of each polygon
+    glDisable(GL_CULL_FACE);                                    //Disable backface culling to render both sides of polygons
+    glShadeModel(GL_SMOOTH);                                    //Set shading to smooth
+    glEnable(GL_TEXTURE_2D);
+    
+    
+    glEnable(GL_COLOR_MATERIAL);                                //Enable color materials
+    //glEnable(GL_LIGHTING);                                      //Enable lighting
+    
+    glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+    
+    glEnable(GL_NORMALIZE);
+    
+    //Register callback functions:
+    glutDisplayFunc(Window::displayCallback);
+    glutReshapeFunc(Window::reshapeCallback);
+    glutIdleFunc(Window::idleCallback);
+    glutKeyboardFunc(Window::keyboardCallback);
+    //glutSpecialFunc(Window::processSpecialKeys);
+    
+    glutMouseFunc(Window::mouseCallback);
+    glutMotionFunc(Window::motionCallback);
+    
+    //Initialize the Window:
+    //The body of this function is a great place to load textures, shaders, etc.
+    //and do any operations/calculations/configurations that only need to happen once.
+    Window::initialize();
+    
+    //Start up the render loop!
+    glutMainLoop();
+    
     return 0;
 }
+
